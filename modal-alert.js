ea19a9f8840436d0138b2177af2e14442c7b2d34
peter-pan-tse <twpptse@gmail.com>
2022-05-11 23:15:12 +0800

    updates

---
+++ modal-alert.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+// Get the modal
+var modal = document.getElementById("modalInfoBox");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("modalinfo1");
+var btn2 = document.getElementById("modalinfo2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
