b8694a9afa4996ccdb0f327f036843de009de83a
peter-pan-tse <twpptse@gmail.com>
2022-05-17 21:58:47 +0800

    updates

--- modal-scoreboard.js
+++ modal-scoreboard.js
@@ -1,12 +1,9 @@
 // JavaScript Document
 // Get the modal
-var modal = document.getElementById("modalInfoBox");
-var modalAlert = document.getElementById("modalAlertBox");
+var modal = document.getElementById("modalScoreboardFilter");
 
 // Get the button that opens the modal
-var btn1 = document.getElementById("modalinfo1");
-var btn2 = document.getElementById("modalinfo2");
-var btn3 = document.getElementById("modalAlert");
+var btn1 = document.getElementById("modalFilter");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -15,13 +12,6 @@ var span = document.getElementsByClassName("close")[0];
 btn1.onclick = function() {
   modal.style.display = "block";
 }
-btn2.onclick = function() {
-  modal.style.display = "block";
-}
-btn3.onclick = function() {
-  modalAlert.style.display = "block";
-}
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
 	modal.style.display = "none";
@@ -35,7 +25,5 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
 	  modal.style.display = "none";
-  } if (event.target == modalAlert) {
-	  modalAlert.style.display = "none";
   }
 }
